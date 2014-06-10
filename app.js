56368b22f504ecf5b5224b2e2c2c22b74cc94cc0
Delian Delchev <delian.delchev@gmail.com>
2014-06-10 17:10:31 +0000

    Views has been removed

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
